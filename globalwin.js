38033ad184a889e120caf243b913c830fe430c47
Federico Bucchi <b.federico@gmail.com>
2014-10-14 10:50:44 -0700

    add costructor unit testing

--- globalwin.js
+++ globalwin.js
@@ -56,3 +56,5 @@ function GlobalWin(options) {
   return this.createGlobalWin(this.options)
 
 }
+
+module.exports.GlobalWin = GlobalWin;
---
+++ test/globalwin.js
@@ -0,0 +1,44 @@
+var assert = require('assert');
+var GlobalWin = require('../globalwin.js').GlobalWin;
+
+describe('Global Win', function(){
+
+  describe('Run Constructor', function(){
+    var globalWin = new GlobalWin();
+
+    it('name should be correct', function(){
+      assert(globalWin.name === 'Global Window', 'Name is not correct');
+    })
+
+    it('subname should be empty', function(){
+      assert(globalWin.subname === '', 'Subname is not empty');
+    })
+
+    it('options should be undefined', function(){
+      assert(globalWin.options === undefined, 'Options is not undefined');
+    })
+
+  })
+
+  describe('Run Constructor with options', function(){
+    var globalWin = new GlobalWin({
+      name: 'Super Global Window',
+      subname: 'Subname of the Super Global Window',
+    });
+
+    it('options should be defined', function(){
+      assert(globalWin.options != undefined, 'Options is undefined');
+    })
+
+    it('name should be the one in the options', function(){
+      assert(globalWin.name === 'Super Global Window', 'Name is not correct');
+    })
+
+    it('subname should be the one in the options', function(){
+      assert(globalWin.subname === 'Subname of the Super Global Window',
+        'Subname is not correct');
+    })
+
+  })
+
+})
